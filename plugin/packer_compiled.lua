f19595a61e827db286a931739898926f467ced05
Andrew Darling <andr3w.darling@gmail.com>
2023-05-29 14:09:26 -0500

    remove buggy plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,11 +84,6 @@ _G.packer_plugins = {
     path = "/Users/firefly/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
-  ["barbar.nvim"] = {
-    loaded = true,
-    path = "/Users/firefly/.local/share/nvim/site/pack/packer/start/barbar.nvim",
-    url = "https://github.com/romgrk/barbar.nvim"
-  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "/Users/firefly/.local/share/nvim/site/pack/packer/start/cmp-buffer",
